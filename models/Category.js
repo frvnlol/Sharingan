b493337fbb60efa7ac339c66dbf75f041f0f76be
frvnci5 <frvncis@outlook.com>
2023-10-03 21:01:47 -0700

    category and product models data

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,31 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    }
   },
   {
     sequelize,
