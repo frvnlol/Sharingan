26b037d01250f15a6dc1c88975bd00c96e29b968
frvnci5 <frvncis@outlook.com>
2023-10-04 21:51:52 -0700

    models data

--- models/Category.js
+++ models/Category.js
@@ -16,21 +16,6 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
     }
   },
   {
